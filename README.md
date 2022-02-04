5bc14a365051074e20d7b0daa601bd38e9d9b836
Cem <tasozgurcem@gmail.com>
2022-02-04 12:05:55 +0300

    create connect.py for queries

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Get and analyze appsync graphql api data
+##  Run graphql query on AppSync with Python
+
+#### In order to run create credentials.py file with your AWS credentials below. 
+
+Go to AWS AppSync -> <your-app-sync-application> -> Settings for credentials.
+
+`APPSYNC_API_ENDPOINT_URL = '<your-api-endpoint-url>'`
+
+`APPSYNC_API_KEY = '<your-api-key>'`
+
+connect.py will connect to your appsync api and return the data in JSON format
+
+simpy run `python connect.py`
+
+#### Note: will add Google Sheets connection later.
--- connect.py
+++ connect.py
@@ -1,35 +1,33 @@
-from aws_requests_auth.boto_utils import BotoAWSRequestsAuth
 import requests
+from credentials import *
 
-APPSYNC_API_KEY = 'inAppsyncSettings'
-APPSYNC_API_ENDPOINT_URL = 'https://aaaaaaaaaaaavzbke.appsync-api.ap-southeast-2.amazonaws.com/graphql'
 
-headers = {
-    'Content-Type': "application/graphql",
-    'x-api-key': APPSYNC_API_KEY,
-    'cache-control': "no-cache",
-}
-query = """{
-    GetUserSettingsByEmail(email: "john@washere"){
-      items {name, identity_id, invite_code}
+def run_query(query):
+    # start a session
+    session = requests.Session()
+
+    response = session.request(
+        url=APPSYNC_API_ENDPOINT_URL,
+        method='POST',
+        headers={'x-api-key': APPSYNC_API_KEY},
+        json={'query': query}
+    )
+    print(f'Response code: {response.status_code}')
+    print(response.json()['data'])
+    return response.json()
+
+
+# define the query
+query = """
+{
+      listUsers{
+        items{
+          name
+          availability
+          email
+          createdAt
+        }
+      }
     }
-}"""
-
-
-def test_stuff():
-    # Use the library to generate auth headers.
-    auth = BotoAWSRequestsAuth(
-        aws_host='aaaaaaaaaaaavzbke.appsync-api.ap-southeast-2.amazonaws.com',
-        aws_region='ap-southeast-2',
-        aws_service='appsync')
-
-    # Create an http graphql request.
-    response = requests.post(
-        APPSYNC_API_ENDPOINT_URL,
-        json={'query': query},
-        auth=auth,
-        headers=headers)
-
-    print(response)
-
-#    response = requests.post(APPSYNC_API_ENDPOINT_URL, data=json.dumps({'query': query}), auth=auth, headers=headers)
+    """
+run_query(query)
